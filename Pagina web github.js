48bbcca623b96fc7eb25b319906029934e8335c1
isabellagomez27 <113604867+isabellagomez27@users.noreply.github.com>
2022-09-15 08:00:36 -0500

    Initial commit

---
+++ Pagina web github.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html/>
+<html>
+<head>
+    <metas charset="UTF-8"/>
+    <title> Ejercicio de Introduccion </title>
+</head>
+<body>
+    <h1>Ejercicio de Introduccion de a HTML</h1>
+    <h2> Isabella Gomez 9ºA</h2>
+    <h3> ✨ Gustos ✨ </h3>
+    <ul> 
+        <li> Gustos Musicales 🎵
+    <div>Desde pequeña me a gustado mucho la musica, en especial el reggaeton. Mis artistas<br> favoritas son: Mora, Feid y Bad bunny. Mi cancion favorita de Feid es: La inocente la<br> cual feid la canta junto a Mora. Y de Bad bunny no seria capaz de elegir una ya que <br> a mi me encanto todo su nuevo album Un Versno Sin Ti. </div> 
+    <br>
+    <a href= "https://www.youtube.com/watch?v=ckuGLT1VkGw">La Inocente</a> <br>
+    <a href= "https://www.youtube.com/watch?v=p38WgakuYDo">Moscow Mule</a><br>
+    <a href= "https://www.youtube.com/watch?v=206atvk4uyE"> 512</a>
+    <br>
+    <br>
+        <li> Gustos Gastronomicos 🥗 </li>
+    <div> Sinceramente yo soy muy selectiva con la comida, la mayoria de cosasa no me gustan.<br> Tampoco me gusta de a mucho el dulce, por ejemplo el helado lo odio,  el chocolate <br> blanco no lo soporto, y el duce en general me da dolor de cabeza. 
+    </div>
+        <br>
+        <li> Gustos Deportivos 🏐 </li>
+        <div> Yo e tenido toda una historia con los deportes, de hecho e practicado la mayoria de <br> los  deportes en los que puedads pensar, pero siempre me e aburrido. Pero los que <br> mas me gustan serian patianaje, y voleibol.</div>
+    </ul>
+
+    <h3> Horario De Clase 📅  </h3>
+    <table> 
+    <tr> 
+    <th>Lunes</th>
+    <th> Martes</th>
+    <th> Miercoles</th>
+    <th> Jueves </th>
+    <th> Viernes </th>  
+    </tr>
+
+    <td> <li> Español </li> 
+    <li> Sociales </li>
+    <li> Ingles </li>
+    <li> Matematicas </li>
+    <li> Advisory</li>
+    <li> Sciencias </li> </td>
+
+
+    <td> <li> Español </li> 
+    <li> Sociales </li>
+    <li> Ingles </li>
+    <li> Matematicas </li>
+    <li> Advisory</li>
+    <li> Sciencias </li> </td>
+
+    <td> <li> Español </li> 
+    <li> Sociales </li>
+    <li> Ingles </li>
+    <li> Matematicas </li>
+    <li> Advisory</li>
+    <li> Sciencias </li> </td>
+
+    <td> <li> Español </li> 
+    <li> Sociales </li>
+    <li> Ingles </li>
+    <li> Matematicas </li>
+    <li> Advisory</li>
+    <li> Sciencias </li> </td>
+
+    <td> <li> Español </li> 
+    <li> Sociales </li>
+    <li> Ingles </li>
+    <li> Matematicas </li>
+    <li> Advisory</li>
+    <li> Sciencias </li> </td>
+</table>                 
+                            
+        
+
